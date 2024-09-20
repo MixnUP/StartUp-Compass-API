a671da16419541ff3c645882dc25336ce67d64fa
MixnUP <adriane.loquinte@gmail.com>
2024-09-20 11:51:27 +0800

    added cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from views import views
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(views, url_prefix="/")
 
 
